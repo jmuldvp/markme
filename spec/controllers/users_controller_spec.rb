98e5dc4640d57af1d348e8636a74be8f502aea03
jmuldvp <jmul.dvp@gmail.com>
2016-11-14 21:22:37 -0700

    CP 9 completed

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:my_user) { User.create!(email: "user@user.com", password: "pas123", confirmed_at: Date.today)}
+  let(:my_topic) { Topic.create!(title: "Test title", user: my_user)}
+  let(:my_bookmark) { my_topic.bookmarks.create!(url: "https://www.google.com/", user: my_user)}
 
   describe "GET #show" do
+    before do
+      sign_in my_user
+    end
+
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
