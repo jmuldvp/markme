ac2f261502a00474a427d32eef51b19164683e7a
jmuldvp <jmul.dvp@gmail.com>
2016-11-17 18:04:09 -0700

    Embedly completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
+require 'embedly'
+require 'json'
+
 module ApplicationHelper
+  def disp_embedly(url)
+    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY'], :user_agent => 'Mozilla/5.0 (compatible; mytestapp/1.0; my@email.com)'
+    obj = embedly_api.oembed :url => url
+    obj[0].marshal_dump
+  end
 end
